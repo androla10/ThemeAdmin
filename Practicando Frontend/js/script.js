a14f709d4f6cc0d5c0e092cfc7103aaadc9522b1
Zeler Benji Villarreal Marcelo <zvillarreal@QUIPUTECH.HQ>
2017-08-03 10:44:07 -0500

    Commit cambiando todo

--- Practicando Frontend/js/script.js
+++ Practicando Frontend/js/script.js
@@ -1,22 +1,9 @@
-
-var activeContenUser = function(){
-	var formContentUser = document.getElementsByClassName("content-user")[0];
-	var formContentUser__name = document.getElementsByClassName("content-user__name")[0];
-	var formContentUser__imageprofile = document.getElementsByClassName("content-user__imagen-profile")[0]
-	verificarClassExist(formContentUser.classList,"content-user--active");
-	verificarClassExist(formContentUser__name.classList,"content-user__name--active");
-	verificarClassExist(formContentUser__imageprofile.classList,"content-user__imagen-profile--active");
-}
-
-var verificarClassExist = function(classList, className){
-	var elementoExist = false;
-	classList.forEach(function(value,index){
-			if(value === className)
-				elementoExist = true;
+$(function(){
+	$(".info-user-inline").on("click",function(){
+		$(this).toggleClass("info-user-inline--active","info-user-inline--active");
 	})
 
-	if(elementoExist)
-		classList.remove(className);
-	else
-		classList.add(className);
-}
+	$(".navigation-spicker").on("click",function(){
+		$(".container").toggleClass("container--aside-active","container--aside-active");
+	});
+});
